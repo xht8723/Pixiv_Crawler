12456e2798a6acab57717d02b8e9d3531a962cc6
xht8723 <xht8723@vip.qq.com>
2016-04-17 03:29:50 -0700

    Update pixiv spider.py

--- Pixiv spider framwork/pixiv spider.py
+++ Pixiv spider framwork/pixiv spider.py
@@ -12,26 +12,27 @@ markeduserurl = 'http://www.pixiv.net/bookmark.php?type=user'
 
 
 
-##Creat cookie for login.
-postdata = urllib.urlencode({'mode':'login', 'pixiv_id':'********','pass':'********','skip':'1'}) ## Your pixiv id and password.
+##Save cookie for login.
+postdata = urllib.urlencode({'mode':'login', 'pixiv_id':'********','pass':'********','skip':'1'}) # Your pixiv id and password.
 cookies = cookielib.MozillaCookieJar('cookie.84')
 handler = urllib2.HTTPCookieProcessor(cookies)
 opener = urllib2.build_opener(handler)
 response = opener.open(posturl,postdata)
-cookies.save(ignore_discard=True, ignore_expires=True)
+cookies.save(ignore_discard = True, ignore_expires = True)
 
 
 
 
 
-##Open your marked user page and find all user id
+##Open your marked user page, find all user id and record them in Marked_users.txt 
 openMarkpage = opener.open(markeduserurl)
-markpage=openMarkpage.read()
-users=re.findall('member\.php\?id=(\d+)', markpage)
+markpage = openMarkpage.read()
+users=re.findall('member\.php\?id=(\d+)', markpage)          #regular expression to find user id in HTML file in certain format.
 marked_user_log = open('Marked_users.txt','w')
+
 duplicate_check=[]
 for everyuser in users:
-	if everyuser not in duplicate_check:
+	if everyuser not in duplicate_check:             #check if user duplicated.
 		marked_user_log.write(everyuser+',')
 		duplicate_check.append(everyuser)
 marked_user_log.close()
@@ -41,16 +42,17 @@ marked_user_log.close()
 
 
 
-##Record all image id needs to download
+##Record all image id needs to download into file illust_id.txt
 Marked_users_illust_id=open('illust_id.txt','w')
 for everyuser in users:
 	userpageurl = 'http://www.pixiv.net/member_illust.php?id=' + everyuser
 	openuserpage = opener.open(userpageurl)
 	userpage = openuserpage.read()
-	illustid=re.findall('/member_illust\.php\?mode=medium&amp;illust_id=(\d+)',userpage)
+	illustid=re.findall('/member_illust\.php\?mode=medium&amp;illust_id=(\d+)',userpage)     #regular expression to find picture id in HTML file in certain format
 	duplicate_check=[]
+	
 	for everyillust in illustid:
-		if everyillust not in duplicate_check:
+		if everyillust not in duplicate_check:                  # check if imageid duplicated
 			Marked_users_illust_id.write(everyillust+',')
 			duplicate_check.append(everyillust)
 Marked_users_illust_id.close()
@@ -59,7 +61,7 @@ Marked_users_illust_id.close()
 
 
 
-##create a list of image id
+##create a list of image id from the file illust_id.txt
 Marked_users_illust_id = open('illust_id.txt','r')
 illusts=Marked_users_illust_id.read()
 illustlist=illusts.split(',')
@@ -75,7 +77,7 @@ for everyid in illustlist:
 	illusturl='http://www.pixiv.net/member_illust.php?mode=medium&illust_id='+everyid
 	openimagepage = opener.open(illusturl)
 	imagepage = openimagepage.read()
-	imageurl = re.findall('data-src="(http://.+)" class="original-image"',imagepage)	
+	imageurl = re.findall('data-src="(http://.+)" class="original-image"',imagepage)              #regular expression to find source link of the picture	
 	openfinal = opener.open(imageurl)
 	filename='id_'+everyid+'.png'
 	urllib.urlretrieve(imageurl[0],filename)
