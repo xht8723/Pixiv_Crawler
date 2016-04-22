05995855212c9d59c6d1a6bc0675338b865af835
Hantian Xiao <xht8723@vip.qq.com>
2016-04-22 11:55:20 -0700

    Added files via upload

--- Pixiv spider framwork/pixiv spider.py
+++ Pixiv spider framwork/pixiv spider.py
@@ -14,7 +14,7 @@ userpageurl = 'http://www.pixiv.net/member_illust.php?id='
 
 ##Creat cookie for login.
 print 'Creating cookie...'
-postdata = urllib.urlencode({'mode':'login', 'pixiv_id':'wawa8723','pass':'Chenyiming504','skip':'1'}) ## Your pixiv id and password.
+postdata = urllib.urlencode({'mode':'login', 'pixiv_id':'*******','pass':'*******','skip':'1'}) ## Your pixiv id and password.
 cookies = cookielib.MozillaCookieJar('cookie.84')
 handler = urllib2.HTTPCookieProcessor(cookies)
 opener = urllib2.build_opener(handler)
@@ -131,6 +131,8 @@ del duplicate_check
 
 
 
+
+
 ##create a list of image id
 Marked_users_illust_id = open('illust_id.txt','r')
 illusts=Marked_users_illust_id.read()
@@ -143,8 +145,13 @@ illustlist.pop()
 
 
 
-##start downloading
 print 'Downloading...'
+cookie84 = open('cookie.84','r').read()
+device_token = re.findall('device_token\s+(.+)\n',cookie84)[0]
+phpsessid = re.findall('PHPSESSID\s+(.+)\n',cookie84)[0]
+module_orders_mypage = re.findall('module_orders_mypage\s+(.+)\n',cookie84)[0]
+p_ab_id = re.findall('p_ab_id\s+(.+)\n',cookie84)[0]
+
 for everyid in illustlist:
 	illusturl='http://www.pixiv.net/member_illust.php?mode=medium&illust_id='+everyid
 	request = urllib2.Request(illusturl)
@@ -154,7 +161,7 @@ for everyid in illustlist:
 	if len(imageurl) == 0:
 		continue	
 	hostnum = re.findall('(i\d\.pixiv\.net)',imageurl[0])
-	referer = 'http://www.pixiv.net/member_illust.php?mode=medium&illust_id='+ everyid
+	referer = 'http://www.pixiv.net/member_illust.php?mode=medium&illust_id=' + everyid
 	data = {
 	
 			'Accept': 'image/webp,image/*,*/*;q=0.8',
@@ -165,7 +172,7 @@ for everyid in illustlist:
 			
 			'Connection':'keep-alive',
 			
-			'Cookie':'p_ab_id=1; device_token=2e626a73bae4d1ab278b48b8fb2a2b18; PHPSESSID=3271470_bfc19558818bfe3737bc000e7ad071fa; module_orders_mypage=%5B%7B%22name%22%3A%22everyone_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22spotlight%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22featured_tags%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22contests%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22following_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22mypixiv_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22booth_follow_items%22%2C%22visible%22%3Atrue%7D%5D; __utmt=1; __utma=235335808.542522510.1459631616.1461174833.1461180741.22; __utmb=235335808.19.10.1461180741; __utmc=235335808; __utmz=235335808.1460079625.3.3.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); __utmv=235335808.|2=login%20ever=yes=1^3=plan=premium=1^5=gender=male=1^6=user_id=3271470=1',
+			'Cookie':'p_ab_id='+p_ab_id+'; device_token='+device_token+'; PHPSESSID='+phpsessid+'; module_orders_mypage='+module_orders_mypage+'; __utmb=235335808.19.10.1461180741; __utmc=235335808; __utmz=235335808.1460079625.3.3.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); __utmv=235335808.|2=login%20ever=yes=1^3=plan=premium=1^5=gender=male=1^6=user_id=3271470=1',
 			
 			'Host': hostnum[0],
 			
@@ -181,7 +188,6 @@ for everyid in illustlist:
 	f = open(filename,'wb')
 	f.write(finalopen.read())
 	f.close()
-
 
 
 
